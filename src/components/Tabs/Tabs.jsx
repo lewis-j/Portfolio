83776536e684932b185361703745b5be929b46f3
Lindsey Jackson <lindseylewisjackson@gmail.com>
2023-01-04 07:19:46 -0800

    prevent tab movement on hover

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -4,15 +4,19 @@ const Tabs = ({ length, current, handleActive, list }) => {
   const tabsList = list ? list : [...Array(length).keys()];
 
   const renderTabs = () => {
-    return tabsList.map((item, idx) => (
-      <div
-        key={`tab:${idx * Math.random()}`}
-        onClick={() => handleActive(idx)}
-        className={current === idx ? styles.tabActive : styles.tab}
-      >
-        {list ? <div className={styles.list}>{item}</div> : idx + 1}
-      </div>
-    ));
+    return tabsList.map((item, idx) => {
+      const tabClass = current === idx ? styles.tabActive : styles.tab;
+      // const tabClass = styles.tab + " " + isActive;
+      return (
+        <div
+          onClick={() => handleActive(idx)}
+          className={tabClass}
+          key={`tab:${idx * Math.random()}`}
+        >
+          {list ? <div className={styles.list}>{item}</div> : idx + 1}
+        </div>
+      );
+    });
   };
   return <div className={styles.container}>{renderTabs()}</div>;
 };
