463bb63eb92b9a5688fc548d426123d89bffae3f
Lindsey Jackson <lindseylewisjackson@gmail.com>
2023-01-13 06:37:17 -0800

    added media for portfolio project slide

--- src/assets/img/projects/portfolio/index.js
+++ src/assets/img/projects/portfolio/index.js
@@ -1,8 +1,21 @@
 import { imageWithTheme } from "../../../../util";
 import home from "./home.png";
 import projects from "./projects.png";
+import readMe from "./readMe.png";
+import readMeExpand from "./readMeExpand.png";
+import about from "./about.png";
+import projectsMobile from "./projectsMobile.png";
+import aboutMobile from "./aboutMobile.png";
 
-const images = [home, projects];
-const imgs = imageWithTheme(images, [home]);
+const images = [
+  home,
+  projects,
+  readMe,
+  readMeExpand,
+  about,
+  projectsMobile,
+  aboutMobile,
+];
+const imgs = imageWithTheme(images, [home, readMeExpand]);
 
 export default imgs;
